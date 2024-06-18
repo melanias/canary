a970fc89b752e5d6ea29fb6e4804cb1c483eb337
Phelipe Melanias <phelipe.melanias@gmail.com>
2024-06-17 22:16:47 -0300

    Stages de level e skill! Comando !food implementado.

--- data/stages.lua
+++ data/stages.lua
@@ -4,85 +4,60 @@
 experienceStages = {
 	{
 		minlevel = 1,
-		maxlevel = 8,
-		multiplier = 7,
+		maxlevel = 300,
+		multiplier = 100
 	},
 	{
-		minlevel = 9,
-		maxlevel = 20,
-		multiplier = 6,
+		minlevel = 301,
+		maxlevel = 600,
+		multiplier = 50
 	},
 	{
-		minlevel = 21,
-		maxlevel = 50,
-		multiplier = 5,
+		minlevel = 601,
+		maxlevel = 800,
+		multiplier = 25
 	},
 	{
-		minlevel = 51,
-		maxlevel = 100,
-		multiplier = 4,
+		minlevel = 801,
+		maxlevel = 1000,
+		multiplier = 20
 	},
 	{
-		minlevel = 101,
-		multiplier = 2,
+		minlevel = 1001,
+		maxlevel = 1100,
+		multiplier = 15
 	},
+	{
+		minlevel = 1101,
+		maxlevel = 1200,
+		multiplier = 10
+	},
+	{
+		minlevel = 1201,
+		maxlevel = 1300,
+		multiplier = 5
+	},
+	{
+		minlevel = 1301,
+		maxlevel = 1500,
+		multiplier = 3
+	},
+	{
+		minlevel = 1501,
+		multiplier = 2
+	}
 }
 
 skillsStages = {
 	{
-		minlevel = 10,
-		maxlevel = 60,
-		multiplier = 15,
-	},
-	{
-		minlevel = 61,
-		maxlevel = 80,
-		multiplier = 10,
-	},
-	{
-		minlevel = 81,
-		maxlevel = 110,
-		multiplier = 6,
-	},
-	{
-		minlevel = 111,
-		maxlevel = 125,
-		multiplier = 4,
-	},
-	{
-		minlevel = 126,
-		multiplier = 2,
-	},
+		minlevel = 0,
+		multiplier = 10
+	}
 }
 
 magicLevelStages = {
 	{
 		minlevel = 0,
-		maxlevel = 60,
-		multiplier = 10,
-	},
-	{
-		minlevel = 61,
-		maxlevel = 80,
-		multiplier = 7,
-	},
-	{
-		minlevel = 81,
-		maxlevel = 100,
-		multiplier = 5,
-	},
-	{
-		minlevel = 101,
-		maxlevel = 110,
-		multiplier = 4,
-	},
-	{
-		minlevel = 111,
-		maxlevel = 125,
-		multiplier = 3,
-	},
-	{
-		minlevel = 126,
-		multiplier = 2,
-	},
+		multiplier = 10
+	}
 }
